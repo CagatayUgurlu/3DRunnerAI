b1b9b735890ca21ecfe9df3f4daee8d233ccea79
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-06-19 21:18:26 +0300

    Added

---
+++ Assets/[Game]/Scripts/Opponent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Opponent : MonoBehaviour
+{
+    public NavMeshAgent OpponentAgent;
+    public GameObject Target;
+
+    private void Start()
+    {
+        OpponentAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        OpponentAgent.SetDestination(Target.transform.position);
+
+    }
+}
