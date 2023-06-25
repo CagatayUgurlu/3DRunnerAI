5fb8f638b7e8fdabf7ec3c86bf84b09c427203e1
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-06-25 12:39:39 +0300

    Added settings and their anims

--- Assets/[Game]/Scripts/UIManager.cs
+++ Assets/[Game]/Scripts/UIManager.cs
@@ -1,10 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    #region Buttons & Button Functions
+    public Animator layoutAnim;
+
+    //Buttons
+    public GameObject settings_Open;
+    public GameObject settings_Close;
+    public GameObject sound_On;
+    public GameObject sound_Off;
+    public GameObject vibration_On;
+    public GameObject vibration_Off;
+    public GameObject information;
+
+    public void PanteonURL()
+    {
+        Application.OpenURL("https://www.panteon.games/");
+    }
+    //Button Functions
+    public void Settings_Open()
+    {
+        settings_Open.SetActive(false); 
+        settings_Close.SetActive(true);
+        layoutAnim.SetTrigger("Slide_In");
+        AudioListener.volume = 0;
+    }
+
+    public void Settings_Close()
+    {
+        settings_Open.SetActive(true);
+        settings_Close.SetActive(false);
+        layoutAnim.SetTrigger("Slide_Out");
+    }
+
+    public void Sound_On()
+    {
+        sound_On.SetActive(false);
+        sound_Off.SetActive(true);
+    }
+
+    public void Sound_Off()
+    {
+        sound_On.SetActive(true);
+        sound_Off.SetActive(false);
+    }
+
+    public void Vibration_On()
+    {
+        vibration_On.SetActive(false);
+        vibration_Off.SetActive(true);
+    }
+
+    public void Vibration_Off()
+    {
+        vibration_On.SetActive(true);
+        vibration_Off.SetActive(false);
+    }
+
+    #endregion
+
+    #region WhiteEffect
+
     public Image whiteEffectImage;
     private int effectControl = 0;
     public IEnumerator WhiteEffect()
@@ -34,6 +96,7 @@ public class UIManager : MonoBehaviour
         }
 
     }
+    #endregion
 
 
 }
